767692b09f88736b7ed1feeab36872bf16217b43
Brian Yeh <brian.yeh@coolbitx.com>
2021-10-02 15:12:04 +0800

    chore: let server can use async function

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,5 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import Config from './config.json';
 import Web3 from 'web3';
 import express from 'express';
@@ -8,6 +9,14 @@ let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
+let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, config.appAddress);
+
+// authorize contract
+async function init () {
+  console.log('>> async init called.');
+}
+init();
 
 
 flightSuretyApp.events.OracleRequest({
